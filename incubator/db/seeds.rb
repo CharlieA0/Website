72db519d4a75de5c37d40cbb2c2520f6845eb40f
Leo Lahti <leo.lahti1@gmail.com>
2016-03-10 20:39:18 +0200

    added better seeding

--- incubator/db/seeds.rb
+++ incubator/db/seeds.rb
@@ -1,12 +1,22 @@
 require 'faker'
 Project.delete_all
 20.times  do |i|
+
+  body = Faker::Hacker.say_something_smart
+  rand(30..60).times do |j|
+    dividend = j % 5
+    if dividend == 0 and rand(0..3) == 3
+      body+="\n\n"
+    end
+    body+=" "+Faker::Hacker.say_something_smart
+  end
+
   Project.create(
     id: i,
     name: Faker::App.name,
     started_at: Faker::Date.between(3000.year.ago, Date.today),
     creator: Faker::StarWars.character,
     cost: Faker::Commerce.price,
-    body: Faker::Hacker.say_something_smart
+    body: body
   )
 end
